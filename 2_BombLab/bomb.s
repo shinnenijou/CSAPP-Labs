fad30cd1f3c88a203c749df9dbec13769cd7488c
shinnenijou <littlesword111@gmail.com>
2025-02-15 12:17:42 +0800

    Phase 1 & Phase 2

--- 2_BombLab/bomb.s
+++ 2_BombLab/bomb.s
@@ -1099,9 +1099,9 @@ Disassembly of section .text:
 
 0000000000400ee0 <phase_1>:
   400ee0:	48 83 ec 08          	sub    $0x8,%rsp
-  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi
-  400ee9:	e8 4a 04 00 00       	call   401338 <strings_not_equal>
-  400eee:	85 c0                	test   %eax,%eax
+  400ee4:	be 00 24 40 00       	mov    $0x402400,%esi      // pass parameter through %esi, then check certain string with input
+  400ee9:	e8 4a 04 00 00       	call   401338 <strings_not_equal> // another parameter passed through %rdi, which is unchanged
+  400eee:	85 c0                	test   %eax,%eax            // explode if not equal, thus the string 0x402400 may be the key
   400ef0:	74 05                	je     400ef7 <phase_1+0x17>
   400ef2:	e8 43 05 00 00       	call   40143a <explode_bomb>
   400ef7:	48 83 c4 08          	add    $0x8,%rsp
@@ -1113,23 +1113,23 @@ Disassembly of section .text:
   400efe:	48 83 ec 28          	sub    $0x28,%rsp
   400f02:	48 89 e6             	mov    %rsp,%rsi
   400f05:	e8 52 05 00 00       	call   40145c <read_six_numbers>
-  400f0a:	83 3c 24 01          	cmpl   $0x1,(%rsp)
+  400f0a:	83 3c 24 01          	cmpl   $0x1,(%rsp)  // start at 1
   400f0e:	74 20                	je     400f30 <phase_2+0x34>
   400f10:	e8 25 05 00 00       	call   40143a <explode_bomb>
   400f15:	eb 19                	jmp    400f30 <phase_2+0x34>
-  400f17:	8b 43 fc             	mov    -0x4(%rbx),%eax
-  400f1a:	01 c0                	add    %eax,%eax
-  400f1c:	39 03                	cmp    %eax,(%rbx)
+  400f17:	8b 43 fc             	mov    -0x4(%rbx),%eax ++++++++ loop jump here!
+  400f1a:	01 c0                	add    %eax,%eax    // double to previous
+  400f1c:	39 03                	cmp    %eax,(%rbx)  // check equals. means current value should be double to the previous
   400f1e:	74 05                	je     400f25 <phase_2+0x29>
   400f20:	e8 15 05 00 00       	call   40143a <explode_bomb>
-  400f25:	48 83 c3 04          	add    $0x4,%rbx
-  400f29:	48 39 eb             	cmp    %rbp,%rbx
-  400f2c:	75 e9                	jne    400f17 <phase_2+0x1b>
+  400f25:	48 83 c3 04          	add    $0x4,%rbx           ------ loop iterate
+  400f29:	48 39 eb             	cmp    %rbp,%rbx            ------ loop condition 
+  400f2c:	75 e9                	jne    400f17 <phase_2+0x1b> ++++++++ loop jump
   400f2e:	eb 0c                	jmp    400f3c <phase_2+0x40>
-  400f30:	48 8d 5c 24 04       	lea    0x4(%rsp),%rbx
-  400f35:	48 8d 6c 24 18       	lea    0x18(%rsp),%rbp
+  400f30:	48 8d 5c 24 04       	lea    0x4(%rsp),%rbx     ------ loop initialization, start at 0x4(%rsp)
+  400f35:	48 8d 6c 24 18       	lea    0x18(%rsp),%rbp    ------ loop initialization, end at 0x18(%rsp)
   400f3a:	eb db                	jmp    400f17 <phase_2+0x1b>
-  400f3c:	48 83 c4 28          	add    $0x28,%rsp
+  400f3c:	48 83 c4 28          	add    $0x28,%rsp ------------- recover stack 
   400f40:	5b                   	pop    %rbx
   400f41:	5d                   	pop    %rbp
   400f42:	c3                   	ret    
@@ -1456,26 +1456,26 @@ Disassembly of section .text:
   401338:	41 54                	push   %r12
   40133a:	55                   	push   %rbp
   40133b:	53                   	push   %rbx
-  40133c:	48 89 fb             	mov    %rdi,%rbx
-  40133f:	48 89 f5             	mov    %rsi,%rbp
+  40133c:	48 89 fb             	mov    %rdi,%rbx              // first parameter %rdi -> rbx
+  40133f:	48 89 f5             	mov    %rsi,%rbp              // second parameter %rsi -> %rbp
   401342:	e8 d4 ff ff ff       	call   40131b <string_length>
   401347:	41 89 c4             	mov    %eax,%r12d
-  40134a:	48 89 ef             	mov    %rbp,%rdi
+  40134a:	48 89 ef             	mov    %rbp,%rdi              // %rbp -> %rdi
   40134d:	e8 c9 ff ff ff       	call   40131b <string_length>
   401352:	ba 01 00 00 00       	mov    $0x1,%edx
-  401357:	41 39 c4             	cmp    %eax,%r12d
+  401357:	41 39 c4             	cmp    %eax,%r12d             // comp two string
   40135a:	75 3f                	jne    40139b <strings_not_equal+0x63>
-  40135c:	0f b6 03             	movzbl (%rbx),%eax
+  40135c:	0f b6 03             	movzbl (%rbx),%eax            // read the first char
   40135f:	84 c0                	test   %al,%al
   401361:	74 25                	je     401388 <strings_not_equal+0x50>
-  401363:	3a 45 00             	cmp    0x0(%rbp),%al
+  401363:	3a 45 00             	cmp    0x0(%rbp),%al 
   401366:	74 0a                	je     401372 <strings_not_equal+0x3a>
   401368:	eb 25                	jmp    40138f <strings_not_equal+0x57>
   40136a:	3a 45 00             	cmp    0x0(%rbp),%al
   40136d:	0f 1f 00             	nopl   (%rax)
   401370:	75 24                	jne    401396 <strings_not_equal+0x5e>
-  401372:	48 83 c3 01          	add    $0x1,%rbx
-  401376:	48 83 c5 01          	add    $0x1,%rbp
+  401372:	48 83 c3 01          	add    $0x1,%rbx // next char
+  401376:	48 83 c5 01          	add    $0x1,%rbp // next char
   40137a:	0f b6 03             	movzbl (%rbx),%eax
   40137d:	84 c0                	test   %al,%al
   40137f:	75 e9                	jne    40136a <strings_not_equal+0x32>
