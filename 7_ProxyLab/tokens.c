b08f9faeacd3ebbbfa16b3956238ccf63b56053f
shinnenijou <littlesword111@gmail.com>
2025-03-07 20:37:00 +0800

    ProxyLab: refactor tokens(do not use heap memory)

--- 7_ProxyLab/tokens.c
+++ 7_ProxyLab/tokens.c
@@ -1,58 +1,13 @@
 #include "csapp.h"
 #include "tokens.h"
 
-tokens_t *create_tokens()
+/*
+ * split_line - split null-terminated string to tokens by given delim
+ * return the number of tokens
+ */
+int split_line(token_t tokens[], size_t max_token, char *usrbuf, size_t maxlen, char delim)
 {
-    tokens_t *tokens = Malloc(sizeof(tokens_t));
-    tokens->dummy.next = &tokens->dummy;
-    tokens->dummy.prev = &tokens->dummy;
-    tokens->dummy.token = NULL;
-    tokens->dummy.size = 0;
-
-    return tokens;
-}
-
-void add_token(tokens_t *tokens, char *buf, size_t n)
-{
-    token_t *t = Malloc(sizeof(token_t));
-    t->token = Malloc(n + 1);
-    memcpy(t->token, buf, n);
-    t->size = n;
-    t->token[n] = '\0';
-
-    token_t *last = tokens->dummy.prev;
-
-    last->next = t;
-    tokens->dummy.prev = t;
-    t->prev = last;
-    t->next = &tokens->dummy;
-}
-
-void free_tokens(tokens_t *tokens)
-{
-    if (!tokens)
-    {
-        return;
-    }
-
-    token_t *p = tokens->dummy.next;
-
-    while (p != &tokens->dummy)
-    {
-        token_t *next = p->next;
-        Free(p->token);
-        Free(p);
-        p = next;
-    }
-
-    Free(tokens);
-}
-
-/* split_line - split null-terminated string to tokens by given delim */
-tokens_t *split_line(char *usrbuf, size_t maxlen, char delim)
-{
-    tokens_t *tokens = create_tokens();
-
+    size_t cnt = 0;
     size_t i = 0;
     size_t start = ~0;
 
@@ -67,15 +22,24 @@ tokens_t *split_line(char *usrbuf, size_t maxlen, char delim)
 
         if (is_delim && start != ~0)
         {
-            add_token(tokens, usrbuf + start, i - start);
+            tokens[cnt].token = usrbuf + start;
+            tokens[cnt].size = i - start;
+            ++cnt;
             start = ~0;
+
+            if (cnt == max_token)
+            {
+                break;
+            }
         }
     }
 
     if (start != ~0)
     {
-        add_token(tokens, usrbuf + start, i - start);
+        tokens[cnt].token = usrbuf + start;
+        tokens[cnt].size = i - start;
+        ++cnt;
     }
 
-    return tokens;
+    return cnt;
 }
--- 7_ProxyLab/tokens.h
+++ 7_ProxyLab/tokens.h
@@ -6,22 +6,12 @@
 /* helper struct */
 
 /* help to parse request and response line */
-typedef struct _token
+typedef struct
 {
     char *token;
     size_t size;
-    struct _token *prev;
-    struct _token *next;
 } token_t;
 
-typedef struct
-{
-    token_t dummy;
-} tokens_t;
-
-tokens_t *create_tokens();
-void add_token(tokens_t *tokens, char *buf, size_t n);
-void free_tokens(tokens_t *tokens);
-tokens_t *split_line(char *usrbuf, size_t maxlen, char delim);
+int split_line(token_t tokens[], size_t max_token, char *usrbuf, size_t maxlen, char delim);
 
 #endif
